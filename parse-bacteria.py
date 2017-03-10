6ad3aee5d5e4287abfc1cfe9a9447684bef2dd73
Eleni Mijalis <elenimijalis@gmail.com>
2017-03-10 13:38:15 -0600

    researching which phyla are gram +/-

--- parse-bacteria.py
+++ parse-bacteria.py
@@ -3,6 +3,49 @@ import argparse
 import json
 import sys
 
+"""
+- Acidobacteria
+- Alphaproteobacteria
+- Betaproteobacteria
+- Gammaproteobacteria
+- Deltaproteobacteria
+- Epsilonproteobacteria
+- Aquificae
+- Cyanobacteria
+- Spirochaetes
+- Thermotogae
+- Bacteroidetes/Chlorobi
+- Planctomycetes
+- Fusobacteria
+- Chlamydiae/Verrucomicrobia
+- Deinococcus-Thermus  # note: some deinococcus are sudo(?)-positive, but no phages have this host anyway
+Actinobacteria
+Firmicutes
+Other Bacteria
+"""
+
+# gram_negative = [
+    # 'Acidobacteria',
+    # 'Alphaproteobacteria',
+    # 'Betaproteobacteria',
+    # 'Gammaproteobacteria',
+    # 'Deltaproteobacteria',
+    # 'Epsilonproteobacteria',
+    # 'Aquificae',
+    # 'Cyanobacteria',
+    # 'Spirochaetes',
+    # 'Thermotogae',
+    # 'Bacteroidetes/Chlorobi',
+    # 'Planctomycetes',
+    # 'Fusobacteria',
+    # 'Chlamydiae/Verrucomicrobia',
+# ]
+
+# gram_positive = [
+# ]
+
+gram_stains = ['+', '-', '']
+
 def get_bacteria(b):
     orgs = {}
     for i, line in enumerate(b):
@@ -10,23 +53,32 @@ def get_bacteria(b):
             continue
 
         l = line.split('\t')
-        if len(l[3].split()) > 1:
-            bact_name = l[3].split()[0] + ' ' + l[3].split()[1]
-        else:
-            bact_name = l[3]
+        if l[4] == 'Archaea' or l[5] == 'Other Bacteria':
+            continue
+
+        # if len(l[3].split()) > 1:
+            # bact_name = l[3].split()[0] + ' ' + l[3].split()[1]
+        # else:
+        bact_name = l[3].split()[0]
+
+        if l[9] not in gram_stains:
+            l[9] = ''
 
         if bact_name not in orgs:
             orgs[bact_name] = {"group": l[5], "gram": l[9]}
         else:
             g = orgs[bact_name]["gram"]
             if g and l[9] and g is not l[9]:
-                print str(i+1) + ": " + bact_name + " gram was " + g + " but another one says " + l[9]
+                pass
+                # print str(i+1) + ": " + bact_name + " gram was " + g + " but another one says " + l[9]
             else:
                 orgs[bact_name] = {"group": l[5], "gram": l[9]}
+
+    return json.dumps(orgs)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='parses ncbi bacteria file to get gram information')
     parser.add_argument('b', type=argparse.FileType("r"), help='bacteria txt file')
     args = parser.parse_args()
 
-    get_bacteria(args.b)
+    print get_bacteria(args.b)
