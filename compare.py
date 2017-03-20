f393c94bff5b0d9e003ea61c26701bbc9dbd46aa
Eleni Mijalis <elenimijalis@gmail.com>
2017-03-20 13:40:35 -0500

    use all bacteria

--- compare.py
+++ compare.py
@@ -14,12 +14,39 @@ def compare_files(ncbi, labrat):
     a = bact_ncbi.viewkeys() & bact_labrat.viewkeys()  # set of same entry
     b = bact_ncbi.viewkeys() ^ bact_labrat.viewkeys()  # set of unique entries
 
-    for i in sorted(list(a)):
+    bact_list = {}
+
+    for i in list(a):
         n = bact_ncbi[i]["gram"]
         l = bact_labrat[i]
         if n == l:
-            print i + '\t' + n
+            bact_list[i] = n
+            # print i + '\t' + n
 
+    for i in list(b):
+        gram = ''
+        if i in bact_ncbi:
+            gram = bact_ncbi[i]["gram"]
+        elif i in bact_labrat:
+            gram = bact_labrat[i]
+
+        bact_list[i] = gram
+
+    final_hosts = {}
+    for b in sorted(bact_list):
+        name = b.strip().split()[0]
+        if bact_list[b] != 'unknown':
+            if name not in final_hosts:
+                final_hosts[name] = bact_list[b]
+            else:
+                if final_hosts[name] != bact_list[b]:
+                    print '****'
+                    print b
+                    print final_hosts[name], bact_list[b]
+                    print '****'
+
+    with open('curated.json', 'w') as final:
+        final.write(json.dumps(final_hosts, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='compares ncbi to thelabrat.com')
