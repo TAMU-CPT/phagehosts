23ca42a24836267e773b53fa2d56f74d11b6e597
Eleni Mijalis <elenimijalis@gmail.com>
2017-04-10 14:08:16 -0500

    modified for refseq output

--- assign_phages.py
+++ assign_phages.py
@@ -5,7 +5,7 @@ import json
 
 
 def assign_gram(phage_file, bacteria_file):
-    phages = json.load(phage_file)
+    # phages = json.load(phage_file)
     bacts = json.load(bacteria_file)
 
     additions = {
@@ -47,25 +47,31 @@ def assign_gram(phage_file, bacteria_file):
     }
 
     hosts = {}
-    for p in phages:
-        host = p['Organism_Name'].split()[0]
+    # for p in phages:
+    for p in phage_file:
+        # host = p['Organism_Name'].split()[0]
+        host = p.strip().split()[0]
 
         if host in bacts:
             pass
         elif host in additions:
             host = additions[host]
         elif host in ambiguous:
-            if 'Lau218' in p['Organism_Name'] or 'Lily' in p['Organism_Name'] or 'phiJL001' in p['Organism_Name'] or 'phiNIH1.1' in p['Organism_Name']:
+            # if 'Lau218' in p['Organism_Name'] or 'Lily' in p['Organism_Name'] or 'phiJL001' in p['Organism_Name'] or 'phiNIH1.1' in p['Organism_Name']:
+            if 'Lau218' in p or 'Lily' in p or 'phiJL001' in p or 'phiNIH1.1' in p:
                 host = ambiguous[host]
-            elif 'APSE-2' in p['Organism_Name']:  # podophage with host Hamiltonella
+            elif 'APSE-2' in p:  # podophage with host Hamiltonella
+            # elif 'APSE-2' in p['Organism_Name']:  # podophage with host Hamiltonella
                 host = 'Enterobacteriaceae'
-            elif 'vB_EcoP_SU10' in p['Organism_Name']:  # podophage with host E. coli
+            elif 'vB_EcoP_SU10' in p:  # podophage with host E. coli
+            # elif 'vB_EcoP_SU10' in p['Organism_Name']:  # podophage with host E. coli
                 host = 'Escherichia'
         else:
             host = 'Unknown'
 
         # types[bacts[host]] += 1
-        print p['Organism_Name'] + '\t' + bacts[host]
+        # print p['Organism_Name'] + '\t' + bacts[host]
+        print p.strip() + '\t' + bacts[host]
 
     # print types
 
