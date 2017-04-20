dbc0ca5a05df04066cc4cb8e41de4d3fee9272d3
Eleni Mijalis <elenimijalis@gmail.com>
2017-04-20 12:27:47 -0500

    output clusters

--- refseqs/parse_meme.py
+++ refseqs/parse_meme.py
@@ -2,17 +2,32 @@ from Bio import motifs
 import argparse
 import sys
 
-def parse_meme(meme):
-    record = motifs.parse(meme, "meme")
-    for motif in record:
+
+def parse_meme(meme_file, fasta_file):
+    seq_names = {}
+    all_motifs = motifs.parse(meme_file, "meme")
+    for motif in all_motifs:
         for i in motif.instances:
-            print i.sequence_name
-        print dir(motif)
-        sys.exit()
+            if i.sequence_name.strip() in seq_names:
+                seq_names[i.sequence_name.strip()].append(motif.name)
+            else:
+                seq_names[i.sequence_name.strip()] = [motif.name]
+
+    clusters = {}
+    for n in seq_names:
+        if str(seq_names[n]) in clusters:
+            clusters[str(seq_names[n])].append(n)
+        else:
+            clusters[str(seq_names[n])] = [n]
+
+    for c in clusters:
+        print c, len(clusters[c])
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='clusters meme results')
     parser.add_argument('meme', type=argparse.FileType("r"), help='meme file')
+    parser.add_argument('fasta', type=argparse.FileType("r"), help='fasta file')
     args = parser.parse_args()
 
-    parse_meme(args.meme)
+    parse_meme(args.meme, args.fasta)
